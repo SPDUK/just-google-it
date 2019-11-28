7bfd6c46bdd4d0ee4723fd3d82137da780c7fd22
Steve P <spdevuk@gmail.com>
2019-11-28 21:17:32 +0000

    feat: add basic google searching and response

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+const googleIt = require('google-it');
 const auth = require('./auth.json');
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(auth.token);
 
-client.on('message', msg => {
-  if (msg.content === 'ping')  msg.reply('pong');
+const options = {};
+
+client.on('message', async msg => {
+  if (msg.content === 'ping') {
+    const [{ title, link }, ...rest] = await googleIt({
+      options,
+      query: msg.content,
+    });
+
+    console.log(title, link);
+
+    Promise.all([
+      msg.reply(
+        `Why didn't you just google it?
+      ${title}
+      ${link}`
+      ),
+      msg.react('🍎'),
+      msg.react('🍊'),
+      msg.react('🍇'),
+    ]);
+  }
 });
